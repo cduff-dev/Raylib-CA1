73d47d8c0b63017ed1c83c5ff0f48cbf9ad8ef02
cduff-dev <charlie.manu@hotmail.com>
2022-11-12 00:05:23 +0000

    Amended and Added Comments. Changes to Positions of Goal Bounding Box. Edited References.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "rlgl.h" //needed for skybox
 #include "raymath.h" //needed for Pitch/Yaw Calculation & application
 
-
+//Enemy Ship Struct
 struct EnemyShip
 {
   Vector3 pos;
@@ -26,7 +26,6 @@ int main(void)
     float pitch = 0.0f;
     float roll = 0.0f;
     bool STcolliding = false;
-    //bool SEcolliding = false;
     bool SE1colliding = false;
     bool SE2colliding = false;
     bool SE3colliding = false;
@@ -36,8 +35,8 @@ int main(void)
     bool GameOver = false;
     bool Alive = true;
     
-    //Enemy Position Vector
-    Vector3 EPosc[3] = {(Vector3){0.0f, 0.0f, -20.0f}, (Vector3){-5.0f,0.0f,-40.0f}, (Vector3){0.0f,0.0f,-60.0f}};
+    //Enemy Position Vector Array
+    Vector3 EPosc[3] = {(Vector3){0.0f, 0.0f, -40.0f}, (Vector3){-5.0f,0.0f,-80.0f}, (Vector3){0.0f,0.0f,-160.0f}};
    
     
     
@@ -48,12 +47,11 @@ int main(void)
      // Music
     //--------------------------------------------------------------------------------------
     InitAudioDevice(); // Initialises Audio Device
-    Music backgroundMusic = LoadMusicStream("resources/BackgroundMusic.mp3");
-    backgroundMusic.looping = true;
-    PlayMusicStream(backgroundMusic);
-    backgroundMusic.looping = true;
+    Music backgroundMusic = LoadMusicStream("resources/BackgroundMusic.mp3"); // Imports Background Music File
+    backgroundMusic.looping = true; //Loops background music
+    PlayMusicStream(backgroundMusic); //Plays Background Music
     
-    //Adds Sound(s)
+    //Import Sound(s)
     Sound EnemyHit = LoadSound("resources/crash.wav");
     Sound ScreamAndExplosion = LoadSound("resources/ScreamAndExplosion.wav");
     Sound Victory = LoadSound("resources/Victory.wav");
@@ -77,31 +75,21 @@ int main(void)
     Track.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = trackTexture;            // Set model diffuse texture
     Vector3 trackPosition = { 0.0f, -1.0f, 0.0f };
     
-
-    //Bounding box for track - based on tracks initial size. may need to scale depending on scale needed for game.
     
     //Player Ship
     Model Ship = LoadModel("Resources/Ship.obj");                   // Loads Ship model from resource folder
     Texture2D shipTexture = LoadTexture("Resources/ShipTextures.png"); // Load model texture
     Ship.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = shipTexture;            // Set model diffuse texture
-    Vector3 shipPosition = { 0.0f, 0.0f, 0.0f };
-    //Bounding box for ship - based on ships initial size. may need to scale depending on scale needed for game.
+    Vector3 shipPosition = { 0.0f, 0.0f, 0.0f }; // Sets Ship Inital Position
     
-    /*
-    //Enemy Ship
-    Model eShip = LoadModel("Resources/Ship.obj");                   // Loads Ship model from resource folder
-    Texture2D eshipTexture = LoadTexture("resources/ShipTextures.png"); // Load model texture
-    eShip.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = eshipTexture;            // Set model diffuse texture
-    //Postion is deteminded by array of vectors EPosc
-    Vector3 eshipPosition = { 0.0f, 0.0f, -20.0f };
-    */
+    
 
-    //Adding Eneny Ships from Sturct
+    //Adding Eneny Ships from Sturct, Adding texure and giving a position to each ship from the EnemyPosition Array
     EnemyShip EnemyShip1;
    
-    EnemyShip1.EModel = LoadModel("Resources/Ship.obj");   
-    EnemyShip1.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("resources/ShipTextures.png");
-    EnemyShip1.pos = {EPosc[0].x,EPosc[0].y,EPosc[0].z};
+    EnemyShip1.EModel = LoadModel("Resources/Ship.obj");   //Loads Model
+    EnemyShip1.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("resources/ShipTextures.png"); //Loads and applys texture to model
+    EnemyShip1.pos = {EPosc[0].x,EPosc[0].y,EPosc[0].z}; //Gets and sets position from EnemyPosition Array
 
     EnemyShip EnemyShip2;
    
@@ -115,10 +103,8 @@ int main(void)
     EnemyShip3.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("resources/ShipTextures.png");
     EnemyShip3.pos = {EPosc[2].x,EPosc[2].y,EPosc[2].z};
 
-    Model Goal = LoadModel("Resources/Goal.obj");                   // Loads Ship model from resource folder
-    //Texture2D GoalTexture = LoadTexture("resources/GoalTextures.png"); // Load model texture
-    //Goal.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = GoalTexture;            // Set model diffuse texture
-    Vector3 GoalPosition = { 1.0f, 0.0f, -250.0f };
+    Model Goal = LoadModel("Resources/Goal.obj");                   // Loads Goal model from resource folder
+    Vector3 GoalPosition = { 1.0f, 0.0f, -450.0f }; //Position vector to set goals Position in game
 
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -154,13 +140,14 @@ int main(void)
             Enemy Struct - Position, Model, Speed, Texture, BoundingBox *
         */
         //----------------------------------------------------------------------------------
-            UpdateMusicStream(backgroundMusic);
+            UpdateMusicStream(backgroundMusic); //Updates music, Needed for music to be audible for more thant 1 frame.
             
         // MOVEMENT 
 
-            //Moves character Right
+        //Character Can only move if the game is NOT over
         if(GameOver== false && Alive == true)
         {
+                //Moves character Right. Checks for D key Press.
                 if(IsKeyDown(KEY_D))
                 {
                     shipPosition.x += 0.3f;
@@ -178,7 +165,7 @@ int main(void)
                     else if (yaw < -0.3f) yaw += 0.5f;
                 }
 
-            //Moves character Left
+            //Moves character Left. Checks for A key Press.
             if(IsKeyDown(KEY_A))
             {
                 shipPosition.x -= 0.3f;
@@ -195,7 +182,7 @@ int main(void)
                 else if (yaw < -0.3f) yaw += 0.5f;
             }
             
-            //Increases characters Speed. Maxes out at 5. Character will always be moving.
+            //Checks for W key Press. Increases characters Speed. Maxes out at 0.5. Character will always be moving.
             if(IsKeyDown(KEY_W))
             {
                  if(characterVelocity<0.5f)
@@ -206,7 +193,7 @@ int main(void)
                  //shipPosition.z -=0.3f;
                  
             }
-            //Decreases Character Speed. Bottoms out at 1. Character will always be moving.
+            //checks for S key press. Decreases Character Speed. Bottoms out at 0.1. Character will always be moving.
             if(IsKeyDown(KEY_S))
             {
                  if(characterVelocity>0.1f)
@@ -217,31 +204,28 @@ int main(void)
                  //shipPosition.z +=0.3f;
                  
             }
-            if(IsKeyDown(KEY_L))
-            {
-                GetFinalTime = true;
-            }
+            
+            
 
         }
-            //applies transformation to yaw of ship.
+        //applies transformation to yaw of ship. Makes it look life the ship is drifting slightly ingame.
         Ship.transform = MatrixRotateXYZ((Vector3){ DEG2RAD*pitch, DEG2RAD*yaw, DEG2RAD*roll });
 
         //Moves Enemy Ship
-        //eshipPosition.z-=0.1f;
         EnemyShip1.pos.z-=0.1f;
         EnemyShip2.pos.z-=0.15f;
         EnemyShip3.pos.z-=0.2f;
 
 
-        //Moves Player Ship
+        //Moves Player Ship foward constantly. 
         shipPosition.z-=characterVelocity;
        
 
         // Draw
         //----------------------------------------------------------------------------------
-        BeginDrawing();
+        BeginDrawing(); //Begins Drawing the Game Scene
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE); //Clears background. Draw a clear white background 
             
             //Cameras position and target are based of the characters position so it always follows the character
             camera.position = (Vector3){ shipPosition.x, shipPosition.y+10.0f, shipPosition.z+10.0f };  // Camera position
@@ -249,19 +233,14 @@ int main(void)
             
 
 
-            BeginMode3D(camera);
-                //Vector3 cubePosition = {characterX, characterY, characterZ};
-                //DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                //DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-                //DrawGrid(10, 1.0f);
+            BeginMode3D(camera); //Sets the camera object i created to be the camera for the game.
+               
                 
                 //Drawing Skybox
-                rlDisableBackfaceCulling();
-                //rlDisableDepthMask();
+                rlDisableBackfaceCulling(); //Disables back face culling so texture can be displayed on the inside of the object.
                     DrawModel(skybox, (Vector3){0, 0, 0}, 500.0f, WHITE);
-                rlEnableBackfaceCulling();
-                //rlEnableDepthMask();
+                rlEnableBackfaceCulling(); //Re-Enables backface culling so only the skybox model has backface culling disables.
+
                 
                 //Draws Ship Model
                 DrawModel(Ship, shipPosition, 1.0f, WHITE);
@@ -272,36 +251,24 @@ int main(void)
                           (Vector3){ shipPosition.x + 2.5f,
                                      shipPosition.y +2.5f,
                                      shipPosition.z + 2.5f }};
-                DrawBoundingBox(shipBounds, YELLOW);
+                DrawBoundingBox(shipBounds, YELLOW); 
                                                            
                
                 //Draws Track model
                 DrawModel(Track, trackPosition, 1.0f, BLUE);
-                 //Creates and draws to screen bounding box around model
+                 //Creates and draws to screen bounding box around model.
                 BoundingBox trackBounds = (BoundingBox){(Vector3){ trackPosition.x - 10.0f,
                                      trackPosition.y,
-                                     trackPosition.z - 600.0f },
+                                     trackPosition.z - 800.0f },
                           (Vector3){ trackPosition.x + 10.0f,
                                      trackPosition.y +1.0f,
                                      trackPosition.z + 10.0f }};
                 DrawBoundingBox(trackBounds, YELLOW); 
 
-                /*
-                //Draws Enemy Ship
-                DrawModel(eShip, eshipPosition, 1.0f, WHITE);
-                //Creates and draws to screen bounding box around model
-                BoundingBox eshipBounds = (BoundingBox){(Vector3){ eshipPosition.x - 2.5f,
-                                     eshipPosition.y,
-                                     eshipPosition.z - 2.5f },
-                          (Vector3){ eshipPosition.x + 2.5f,
-                                     eshipPosition.y +2.5f,
-                                     eshipPosition.z + 2.5f }};
-                DrawBoundingBox(eshipBounds, YELLOW);
-                */
 
                 //Draws GoalModel
                 DrawModel(Goal, GoalPosition, 2.4f, YELLOW);
-                BoundingBox GoalBounds = (BoundingBox){(Vector3){ GoalPosition.x - 10.0f,
+                BoundingBox GoalBounds = (BoundingBox){(Vector3){ GoalPosition.x - 12.0f,
                                      GoalPosition.y,
                                      GoalPosition.z + 5.0f },
                           (Vector3){ GoalPosition.x + 10.0f,
@@ -348,6 +315,7 @@ int main(void)
             //Draws Games FPS in top left of screen
             DrawFPS(10, 10);
 
+            //Checks if player is colliding with track, Draws text if they are. Debug Code.
             if(STcolliding == true)
             {
                 DrawText("Colliding /w Track", 10, 70, 20, ORANGE);
@@ -359,7 +327,7 @@ int main(void)
             {
                 shipPosition.y-=0.4f;
             }
-
+            //Checks if you are colliding with the enemy while the game is not over. (Loss Condition)
             if((SE1colliding == true || SE2colliding == true|| SE3colliding == true) && Alive == true && GameOver == false)
             {
                 DrawText("Colliding /w Enemy", 10, 90, 20, ORANGE);
@@ -369,41 +337,42 @@ int main(void)
                 
                 
             }
+            //Checks if you are colliding with the Gate and the game is not over (Win Condition). Stops the characters velocity once they hit the win condition.
             if(SGcolliding == true && Alive == true && GameOver == false)
             {
                 GetFinalTime = true;
-                PlaySoundMulti(Victory);
-                characterVelocity=0.0f;
+                PlaySoundMulti(Victory); //Plays Win Condition Sound
+                characterVelocity=0.0f; // Sets character velocity to 0. Stops movement once game is over.
                 
             }
-
+            //Checks if character is alive (Loss Condition) Draw text on screen to let player know they lost
             if(Alive == false)
             {
-                DrawText("You Lose", 270, 70, 50, RED);
+                DrawText("You Lose", 270, 70, 50, RED); //Draws loss condition text
             }
 
-            //kill trigger if you fall off track
+            //kill trigger if you fall off track (Loss Condition)
             if(shipPosition.y<-10.0f && Alive == true)
             {
                 Alive = false;
-                PlaySoundMulti(ScreamAndExplosion);
+                PlaySoundMulti(ScreamAndExplosion); // Plays sound.
                 
             }
-            
+            //Draws elaped time on screen durign gameplay. Stops drawing once the game is over.
             if(GetFinalTime == false && GameOver == false)
             {
              DrawText(TextFormat("Elapsed Time: %02.02f", GetTime()), 10, 370, 30, ORANGE);
             }
-
+            //When get final time is true this code gets the elpased time of the game once and then stores it in a variable while putting the game in it gameover state
             if(GetFinalTime == true)
             {
-                finalTime = GetTime();
+                finalTime = GetTime(); //Function used to get elapsed time in application. Stored to be displayed on victory screen.
                 
                 GetFinalTime = false;
                 GameOver = true;
                 
             }
-            
+            //Win Condition. Triggers when player makes it to goal post and is stil alive. Draws Win con text and final time of the player on screen.
             if(GetFinalTime == false && GameOver == true && Alive == true)
             {
                 //Draws Final Time On Screen
@@ -413,22 +382,24 @@ int main(void)
             
             //check if music is being played
             musicPlaying = IsMusicStreamPlaying(backgroundMusic);
+            //Draws text to screen. 1 = Music is playing, 0 = No Music playing. Used to Debug Background Music
             DrawText(TextFormat("Music Playing: %i", musicPlaying), 10, 330, 30, ORANGE);
 
              // Collision
             //----------------------------------------------------------------------------------
             //Checks Collision between bounding boxes track and ship
             STcolliding = CheckCollisionBoxes(shipBounds, trackBounds);    
-            //SEcolliding = CheckCollisionBoxes(shipBounds, eshipBounds);  
+            //Checks Collision between bounding boxes ship and enemy ship(s)
             SE1colliding = CheckCollisionBoxes(shipBounds, eship1Bounds);
             SE2colliding = CheckCollisionBoxes(shipBounds, eship2Bounds);  
             SE3colliding = CheckCollisionBoxes(shipBounds, eship3Bounds);    
+            //Checks Collision between bounding boxes ship and goal
             SGcolliding = CheckCollisionBoxes(shipBounds, GoalBounds);                     
 
             
         
 
-        EndDrawing();
+        EndDrawing(); //Ends Drawing to Scene
 
          
         //----------------------------------------------------------------------------------
@@ -441,11 +412,13 @@ int main(void)
     CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
     UnloadTexture(trackTexture); // unloads Texture of Track
     UnloadTexture(shipTexture); // unloads Texture of Ship
-    //UnloadTexture(eshipTexture); // unloads Texture of Enemy Ship
+    UnloadTexture(EnemyShip1.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture); //Unloads Texture of Enemy Ship 1
+    UnloadTexture(EnemyShip2.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture); //Unloads Texture of Enemy Ship 2
+    UnloadTexture(EnemyShip3.EModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture); //Unloads Texture of Enemy Ship 3
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
-    return 0;
+    return 0; //End of Program
 }
 
 //References
@@ -457,13 +430,10 @@ int main(void)
     Displaying text in raylib using textformat (Uses %i for intergers while i use %f for floats) - Used for drawing the characters speed on screen.
     https://stackoverflow.com/questions/53500874/how-to-show-variables-in-text-in-raylib
 
-    Raylib Cheat sheet was referenced for how certain functions in Raylib Work
+    Raylib Cheat sheet was referenced for what functions were available in Raylib and how they Work
     https://www.raylib.com/cheatsheet/cheatsheet.html
 
-    Raylib pitch/Yaw Example was referenced to add rotation to ship - refactored for my own use
-    https://www.raylib.com/examples.html
-
-    Raylib Model loading example was referenced for adding models and their textures
+    Raylib Example page was referenced for how certain functions worked (Sounds, Background Music, Adding Models / Textures, Pitch/Yaw, Skybox)
     https://www.raylib.com/examples.html
 
     Crash Sound Effect
@@ -486,6 +456,18 @@ int main(void)
     *Notes*
     Remove materials in Blender before exporting models in .obj, Keeping the material info messes up the models in raylib*
     Music is implemented and should be working correctly. - Music is playing according to IsMusicStreamPlaying() but nothing can be heard. Sounds work tho.
+    Music REQUIRES the updateMusicStream() function. this was why i could not hear music as it was only playing music on the first frame. 
+    
+
+    Game Controls:
+    W - Speed up
+    S - Slow down
+    A - Turn Left
+    D - Turn Right
+
+    Objective: Race to the Finish
+    Win Con: Make it to the Goal
+    Loss Con: Fall off track / Collide with Other Ships
 
 
 
